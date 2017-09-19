baeaca70c0ce36a5b0acb32fe7f5a0ac4748ee4e
pakholeung <pakholeung37@gmail.com>
2017-09-20 00:25:59 +0800

    完成关于用户注册登录部分，接下来开展文件上传的工作

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -14,6 +14,7 @@ class User extends CI_Controller
         $this->load->model('users_model');
         $this->load->helper('url');
         $this->load->library('session');
+        $this->cors();
     }
 
     public function index(){
@@ -30,7 +31,7 @@ class User extends CI_Controller
 
 
         //检查是否存在用户
-        $message = $this->check($user);
+        $message = $this->register_check($user);
         if($message == 'OK'){
             //合法，可以注册
             $result = $this->users_model->create_user($user);
@@ -48,21 +49,69 @@ class User extends CI_Controller
     }
 
     public function login(){
+        $user['email'] = $this->input->post('email');
+        $user['password'] = $this->input->post('password');
 
+        $message = $this->login_check($user);
+        if($message == 'OK'){
+            //合法,登录
+            $json = array('status'=> 1,
+                'message'=> $message);
+            echo json_encode($json);
+
+            //设置session
+            $user = $this->users_model->get_user($user['email']);
+
+            $_SESSION['email']      = $user['email'];
+            $_SESSION['nickname']     = $user['nickname'];
+            $_SESSION['logged_in']    = true;
+            $_SESSION['role']     = $user['role'];
+
+        }
+        else {
+            //密码错误或账户不存在
+            $json = array('status'=> 0,
+                'message'=> $message);
+            echo json_encode($json);
+        }
     }
 
     public function logout(){
         //销毁session
         $this->session->sess_destroy();
+        $json = array('status'=> 1,
+            'message'=> 'OK');
+        echo json_encode($json);
     }
 
 
+    public function is_login(){
+        if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+            $json = array(
+                'status'=> 1,
+                'message'=> 'logged_in',
+                'user' => array(
+                    'email' => $_SESSION['email'],
+                    'nickname' => $_SESSION['nickname']),
+            );
+            echo json_encode($json);
+        }
+        else {
+            $json = array(
+                'status'=> 0,
+                'message'=> 'didnt log in'
+            );
+            echo json_encode($json);
+        }
+
+    }
     /*
      *
      *  以下是内部使用的函数，不属于向外暴露的接口
      *
      */
-    private function check($user){
+
+    private function register_check($user){
         $message = NULL;
         if($this->users_model->user_exist($user)){
             $message = 'user exist';
@@ -76,11 +125,22 @@ class User extends CI_Controller
         return $message;
     }
 
-    private function is_login(){
-        if($this->session->userdata('session_id')){
-
-        } else {
-
+    private function login_check($user)
+    {
+        $message = NULL;
+        if(!$this->users_model->user_exist($user)){
+            $message = 'user not exist';
+            return $message;
         }
+        if($this->users_model->password_incorrect($user)){
+            $message = 'password incorrect';
+            return $message;
+        }
+        $message = 'OK';
+        return $message;
+    }
+    private function cors(){
+        header("Access-Control-Allow-Origin: * ");
+        header("Access-Control-Allow-Method:POST,GET,PATCH,PUT,OPTIONS");//允许的方法
     }
 }
